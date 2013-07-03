9bacccd0a16a1fa4780d2bfb38fc78294c2d725e
An Ha <zero579911@gmail.com>
2013-07-03 11:04:01 -0400

    Fixed not being able to find GLFW3 on linux, windows might not work (have not tested it)

--- FindGLFW3.cmake
+++ FindGLFW3.cmake
@@ -2,25 +2,25 @@
 # Try to find GLFW library and include path.
 # Once done this will define
 #
-# GLFW_FOUND
-# GLFW_INCLUDE_PATH
-# GLFW_LIBRARY
+# GLFW3_FOUND
+# GLFW3_INCLUDE_PATH
+# GLFW3_LIBRARY
 # 
 
 IF(WIN32)
-    FIND_PATH( GLFW_INCLUDE_PATH GLFW/glfw3.h
+    FIND_PATH( GLFW3_INCLUDE_PATH GLFW/glfw3.h
 		$ENV{PROGRAMFILES}/GLFW/include
 		${GLFW_ROOT_DIR}/include
 		DOC "The directory where GLFW/glfw3.h resides")
 
-    FIND_LIBRARY( GLFW_LIBRARY
+    FIND_LIBRARY( GLFW3_LIBRARY
         NAMES glfw3 GLFW
         PATHS
         $ENV{PROGRAMFILES}/GLFW/lib
 		${GLFW_ROOT_DIR}/lib
         DOC "The GLFW library")
 ELSE(WIN32)
-	FIND_PATH( GLFW_INCLUDE_PATH GLFW/glfw3.h
+	FIND_PATH( GLFW3_INCLUDE_PATH GLFW/glfw3.h
 		/usr/include
 		/usr/local/include
 		/sw/include
@@ -29,8 +29,8 @@ ELSE(WIN32)
 		DOC "The directory where GLFW/glfw3.h resides")
 
 	# Prefer the static library.
-	FIND_LIBRARY( GLFW_LIBRARY
-		NAMES libGLFW.a GLFW
+	FIND_LIBRARY( GLFW3_LIBRARY
+        NAMES libGLFW.a GLFW libGLFW3.a GLFW3 libglfw.so libglfw.so.3 libglfw.so.3.0
 		PATHS
 		/usr/lib64
 		/usr/lib
@@ -42,9 +42,9 @@ ELSE(WIN32)
 		DOC "The GLFW library")
 ENDIF(WIN32)
 
-SET(GLFW_FOUND "NO")
-IF(GLFW_INCLUDE_PATH AND GLFW_LIBRARY)
-	SET(GLFW_LIBRARIES ${GLFW_LIBRARY})
-	SET(GLFW_FOUND "YES")
+SET(GLFW3_FOUND "NO")
+IF(GLFW3_INCLUDE_PATH AND GLFW3_LIBRARY)
+	SET(GLFW_LIBRARIES ${GLFW3_LIBRARY})
+	SET(GLFW3_FOUND "YES")
     message(STATUS "Found GLFW")
-ENDIF(GLFW_INCLUDE_PATH AND GLFW_LIBRARY)
+ENDIF(GLFW3_INCLUDE_PATH AND GLFW3_LIBRARY)
